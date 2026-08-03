9c48d5c360d0508e39a735fb6b07303d175cc3df
ele-na2011 <1yangele@hdsb.ca>
2026-08-03 01:29:57 +0000

    updating format and stuff

--- src/main.js
+++ src/main.js
@@ -5,8 +5,18 @@ document.querySelector("#app").innerHTML = "<p>loading...</p>";
 fetch(`https://api.nasa.gov/planetary/apod?api_key=${API_KEY}`)
   .then((response) => response.json())
   .then(data => {
+
+    let media;
+
+      if (data.media_type === "image") {
+        media = `<img src="${data.url}"/ style = "max-width: 100%; height: auto;">`;
+      } else if (data.url.includes("youtube.com")) {
+        media = `<iframe width="560" height="315" src="${data.url}" frameborder="0" allowfullscreen></iframe>`;
+      } else {
+        media = `<video src="${data.url}" controls style = "max-width: 100%; height: auto;"></video>`;
+      }
     document.querySelector("#app").innerHTML = `
-      <h1>${data.title}</h1>
-      <img src="${data.url}" alt="${data.title}" />
-      <p>${data.explanation}</p>
+      <h1 class = "title">${data.title}</h1>
+      ${media}
+      <p class = "explanation">${data.explanation}</p>
     `})
